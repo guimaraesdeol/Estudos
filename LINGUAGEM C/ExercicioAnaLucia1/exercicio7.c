59a8c7751a9c11a7a251440ee4adb3b9e83e2c68
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2024-08-14 01:35:08 -0400

    finalexercicios

--- LINGUAGEM C/ExercicioAnaLucia1/exercicio7.c
+++ LINGUAGEM C/ExercicioAnaLucia1/exercicio7.c
@@ -1,5 +1,16 @@
 # include <stdio.h> // exercicio 7
 int main()
 {
-    
+    float metros, decimetros, centimetros, milimetros;
+    printf("Escreva o valor em metros: ");
+    scanf("%f",&metros);   
+    decimetros = metros * 10;
+    centimetros = decimetros * 10;
+    milimetros = centimetros * 10;
+    printf("=====================\n");
+    printf("      Resultados     \n");
+    printf("=====================\n");
+    printf("Valor em decimetros: %.1f\n", decimetros);
+    printf("Valor em centimetros: %.1f\n",centimetros);
+    printf("Valor em milimetros: %.1f\n", milimetros);
 }
