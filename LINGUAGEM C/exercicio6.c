e8452add47abfc1f1424fad3685958adc1d73e09
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2024-08-14 00:58:44 -0400

    update

--- LINGUAGEM C/exercicio6.c
+++ LINGUAGEM C/exercicio6.c
@@ -1,1 +1,16 @@
 #include <stdio.h> //  exercicio numero 6
+int main()
+{
+    float valor, euro, peso, iene;
+    printf("Qual o valor em reais (R$) para conversao? \nR: ");
+    scanf("%f", &valor);
+    euro = (valor/6);
+    peso = (valor*171.04);
+    iene = (valor*26.91);
+    printf("==================\n");
+    printf("TABELA CONVERTIDA\n");
+    printf("==================\n");
+    printf("Valor em euro: $%.2f\n", euro);
+    printf("Valor em pesos argentinos: %.2f\n", peso);
+    printf("Valor em ienes: %.2f\n", iene);
+}
