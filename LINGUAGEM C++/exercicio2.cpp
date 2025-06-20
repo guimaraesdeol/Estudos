d92d04044ee2a3ef600b96126b6057a30771d8a9
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2025-06-20 16:38:54 -0400

    ex

--- LINGUAGEM C++/exercicio2.cpp
+++ LINGUAGEM C++/exercicio2.cpp
@@ -1,3 +1,5 @@
+//Crie um programa que contenha uma sub-rotina para validar a entrada de caracteres. Somente as letras “A”, “a”, “P” ou “p” são permitidas. Caso o usuário informe um caractere diferente, a função deve retornar falso e solicitar uma nova entrada. Informe ao usuário se o caractere foi aceito ou não.
+
 #include <iostream>
 using namespace std;
 
