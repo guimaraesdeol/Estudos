9bf9d247f687b53d4a701680766808af5c86500a
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2025-06-20 16:54:05 -0400

    a

---
+++ LINGUAGEM C++/exercicio3.cpp
@@ -0,0 +1,24 @@
+//Faça um programa que tenha uma sub-rotina que receba dois números inteiros. A sub-rotina deve calcular e exibir a soma dos dois números, além da subtração e da divisão do maior pelo menor. Certifique-se de que os números informados não sejam iguais.
+
+#include <iostream>
+using namespace std;
+
+//int calcular(int num1, int num2);
+
+int main(){
+    int num1, num2;
+    cout<<"Digite o primeiro numero inteiro: ";
+    cin>>num1;
+    cout<<"Digite o segundo numero inteiro: ";
+    cin>>num2;
+ //   calcular(num1, num2);
+
+
+
+}
+
+//int calcular(int num1, int num2){
+    //Soma
+//    cout << "Soma: " << num1+num2 << endl;
+ //   return 0;
+//}
